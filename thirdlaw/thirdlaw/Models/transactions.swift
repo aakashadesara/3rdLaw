49148ec1c3f997e96035a348db39c1e256cb141b
bjp12 <bjp12@berkeley.edu>
2019-03-31 02:28:09 -0700

    try this again

---
+++ thirdlaw/thirdlaw/Models/transactions.swift
@@ -0,0 +1,9 @@
+//
+//  transactions.swift
+//  thirdlaw
+//
+//  Created by Bijal Patel on 3/31/19.
+//  Copyright © 2019 Ajay Merchia. All rights reserved.
+//
+
+import Foundation
---
+++ thirdlaw/thirdlaw/ViewControllers/MainActivities/SignUp/SignUpVC-logic.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpVC-logic.swift
+//  thirdlaw
+//
+//  Created by Bijal Patel on 3/31/19.
+//  Copyright © 2019 Ajay Merchia. All rights reserved.
+//
+// Logic - Functions relating to the core functionality/value prop of the application.
+
+import Foundation
+import UIKit
+import ARMDevSuite
+
+extension SignUpVC {
+    @objc func buttonClicked(){
+        
+    }
+    
+
+}
